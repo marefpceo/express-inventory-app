f6cdf560736988c7fe78843e1b6c0fd344453f33
marefpceo <marefpceo@gmail.com>
2024-02-06 14:00:29 -0700

    Completed Category and Subcategory forms

--- controllers/subCategoryController.js
+++ controllers/subCategoryController.js
@@ -1,4 +1,5 @@
 const SubCategory = require('../models/subCategory');
+const Category = require('../models/category');
 const Item = require('../models/item');
 const asyncHandler = require('express-async-handler');
 
@@ -11,6 +12,7 @@ exports.sub_category_list = asyncHandler(async (req, res, next) => {
     subcategory_list: allSubcategories,
   });
 });
+
 
 // Display detail page for a specific Category
 exports.sub_category_detail = asyncHandler(async (req, res, next) => {
@@ -26,30 +28,41 @@ exports.sub_category_detail = asyncHandler(async (req, res, next) => {
   });
 });
 
+
 // Display Category create form on GET
 exports.sub_category_create_get = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Sub Category create GET');
+  const allCategories = await Category.find().sort({ name: 1 });
+
+  res.render('sub_category_form', {
+    title: 'Create Subcategory',
+    categories: allCategories,
+  });
 });
+
 
 // Handle Category create on POST
 exports.sub_category_create_post = asyncHandler(async (req, res, next) => {
   res.send('NOT IMPLEMENTED: Sub Category create POST');
 });
 
+
 // Display Category update form on GET
 exports.sub_category_update_get = asyncHandler(async (req, res, next) => {
   res.send('NOT IMPLEMENTED: Sub Category update GET');
 });
+
 
 // Handle Category update form on POST
 exports.sub_category_update_post = asyncHandler(async (req, res, next) => {
   res.send('NOT IMPLEMENTED: Sub Category update POST');
 });
 
+
 // Display Category delete form on GET
 exports.sub_cateogry_delete_get = asyncHandler(async (req, res, next) => {
   res.send('NOT IMPLEMENTED: Sub Category delete GET');
 });
+
 
 // Handle Category delete on POST
 exports.sub_category_delete_post = asyncHandler(async (req, res, next) => {
