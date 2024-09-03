b1e9b0886bed007a84a791c236f50ff3d2a6eb3d
marefpceo <32721274+marefpceo@users.noreply.github.com>
2024-09-03 15:40:29 -0700

    Updating local with remote
    Currently working category item list pages

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -24,10 +24,8 @@ exports.index = asyncHandler(async (req, res, next) => {
 
 // Displays list of all Categories
 exports.category_list = asyncHandler(async (req, res, next) => {
-  // const allCategories = await Category.find().sort({ name: 1 }).exec();
   const allCategories = await db.getCategoryNames();
 
-  console.log(allCategories);
   res.render('category_list', {
     title: 'Category List',
     category_list: allCategories,
